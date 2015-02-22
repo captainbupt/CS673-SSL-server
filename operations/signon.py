e1b1d115240749db5614869692c91310c3806866
weizhou <weizhouh@Bu.edu>
2015-02-21 23:26:57 -0500

    signon

--- operations/signon.py
+++ operations/signon.py
@@ -2,6 +2,7 @@ import json,log,constant
 
 INFO_USERNAME = 'username'
 INFO_PASSWORD = 'passwd'
+INFO_NICKNAME = 'nickname'
 RESULT_USERID = 'userid'
 RESULT_NICKNAME = 'nickname'
 RESULT_CREDIT = 'credit'
@@ -9,8 +10,8 @@ RESULT_RANK = 'rank'
 RESULT_RECORDS = 'records'
 RESULT_BALANCE = 'balance'
 
-def verify(username, password):
-	if isinstance(username,str) and isinstance(password,str):
+def verify(username, password, nickname):
+	if isinstance(username,str) and isinstance(password,str) and isinstance(nickname,str):
 		return False
 	return True
 
@@ -24,21 +25,24 @@ def getUserInfo(username):
 	result[RESULT_BALANCE] = [12.34]
 	return result
 
-def login(data):
+def signon(data):
 	statu = constant.STATUS_SUCCESS
 	result = {}
 	username = None
 	password = None
+	nickname = None
 	if INFO_USERNAME in data:
 		username = data[INFO_USERNAME]
 	if INFO_PASSWORD in data:
 		password = data[INFO_PASSWORD]
-	if username is None or password is None:
-		log.warning('login: missing username or password')
+	if INFO_NICKNAME in data:
+		nickname = data[INFO_NICKNAME]
+	if username is None or password is None or nickname is None:
+		log.warning('signon: missing username, password or nickname')
 		statu = constant.STATUS_PARAMETER_UNMATCHED
 		return statu,result
 	if not verify(username, password):
-		log.warning('login: unmatched username or password')
+		log.warning('signon: unmatched username or password')
 		statu = constant.STATUS_INFORMATION_INVALID
 		return statu,result
 	return statu, getUserInfo(username)
